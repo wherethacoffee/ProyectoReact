3eef45f7bb485dfc578f0e198ff60f504834ef55
wherethacoffee <whr.thacoff23@gmail.com>
2023-10-10 20:50:15 -0600

    Se actualizó el archivo de inicio de backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,30 @@
 const express = requiere('express');
+const morgan = require('morgan');
+const mysql = require('mysql');
+const myConnection = require('express-myconnection');
+
+const app = express();
+
+//importing routes
+const ticketRoutes = require('./routes/ticketRoutes');
+
+//Settings
+app.set('port', process.env.PORT || 4000);
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(myConnection(mysql, {
+    host: 'localhost',
+    user: 'root',
+    password: 'admin',
+    port: 3306,
+    database: 'ticket'
+}, 'single')
+);
+
+app.use(express.urlencoded({extended: false}));
+
+// starting the server
+app.listen(app.get('port'), () => {
+    console.log('listening on port 3000');
+});
