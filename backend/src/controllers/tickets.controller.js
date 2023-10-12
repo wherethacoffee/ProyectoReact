3478bbca1572dd1f1cf21a73aa75f7e8fa53a7c4
wherethacoffee <whr.thacoff23@gmail.com>
2023-10-11 22:14:07 -0600

    Se agregaron los metodos del crud de la api

--- backend/src/controllers/tickets.controller.js
+++ backend/src/controllers/tickets.controller.js
@@ -1,16 +1,53 @@
 
 export const save = (req, res) => {
-    res.send("registrado");
-}
+    const data = req.body;
+
+    console.log(data);
+    req.getConnection((err, conn) => {
+        conn.query('INSERT INTO turno SET ?', [data], (err, turno) => {
+            if (err) {
+                res.json(err);
+            }
+            res.send('Registrando')
+        });
+    });
+};
 
 export const list = (req, res) => {
-    res.send("listado");
-}
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM turno', (err, turnos) => {
+            if (err) {
+                res.json(err);
+            }
+            res.json(turnos);
+        });
+    });
+};
 
 export const remove = (req, res) => {
-    res.send("eliminado");
+    const { id } = req.params;
+
+    req.getConnection((err, conn) => {
+        conn.query('DELETE FROM turno WHERE id = ?', [id], (err, rows) => {
+            if (err) {
+                res.json(err);
+            }
+            res.send('Eliminando')
+        });
+    });
 }
 
 export const update = (req, res) => {
-    res.send("actualizado");
+    const { id } = req.params;
+    const newTurno= req.body;
+    console.log(newTurno);
+    req.getConnection((err, conn) => {
+        conn.query('UPDATE turno set ? WHERE id = ?', [newTurno, id], (err, rows) => {
+            if (err) {
+                res.json(err);
+            }
+            res.send('Actualizando')
+            res.json(rows);
+        });
+    });
 }
