a26fb64ccc9abcc1ffa2b9e21fa84d6cfe024eda
Ricardo RG <rizok.rdz@hotmail.com>
2023-10-11 20:16:26 -0600

    cambio

--- backend/db/script.sql
+++ backend/db/script.sql
@@ -1,55 +1,64 @@
-CREATE TABLE representante(
-  id INT NOT NULL,
-  nombre TEXT,
-  celular TEXT,
-  telefono TEXT,
-  correo TEXT,
-  PRIMARY KEY(id)
+CREATE TABLE representante (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nombre TEXT NOT NULL,
+  celular TEXT NOT NULL,
+  telefono TEXT NOT NULL,
+  correo TEXT NOT NULL
 );
 
-CREATE TABLE turno(
-  id INT NOT NULL,
+CREATE TABLE turno (
+  id INT AUTO_INCREMENT PRIMARY KEY,
   id_representante INT NOT NULL,
   id_municipio INT NOT NULL,
   id_status INT NOT NULL,
   id_asunto INT NOT NULL,
   curp_alumno TEXT NOT NULL,
-  PRIMARY KEY(id)
+  FOREIGN KEY (id_representante) REFERENCES representante(id),
+  FOREIGN KEY (id_municipio) REFERENCES municipio(id),
+  FOREIGN KEY (id_status) REFERENCES `status`(id),
+  FOREIGN KEY (id_asunto) REFERENCES asunto(id),
+  FOREIGN KEY (curp_alumno) REFERENCES alumno(curp)
 );
 
-CREATE TABLE nivel(
-  id INT NOT NULL,
-  descripcion TEXT,
-  nivel_id INT NOT NULL,
-  PRIMARY KEY(id)
+CREATE TABLE nivel (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  descripcion TEXT NOT NULL,
+  nivel_id INT NOT NULL
 );
 
-CREATE TABLE asunto(id INT NOT NULL, descripcion TEXT, PRIMARY KEY(id));
+CREATE TABLE asunto (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  descripcion TEXT NOT NULL
+);
 
-CREATE TABLE `status`(id INT NOT NULL, descripcion TEXT, PRIMARY KEY(id));
+CREATE TABLE `status` (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  descripcion TEXT NOT NULL
+);
 
-CREATE TABLE municipio(
-  id INT NOT NULL,
-  nombre TEXT,
+CREATE TABLE municipio (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nombre TEXT NOT NULL,
   estado_id INT NOT NULL,
-  PRIMARY KEY(id)
+  FOREIGN KEY (estado_id) REFERENCES estado(id)
 );
 
-CREATE TABLE estado(id INT NOT NULL, nombre TEXT, PRIMARY KEY(id));
-
-CREATE TABLE alumno(
-  curp TEXT NOT NULL,
-  nombre TEXT,
-  materno TEXT,
-  paterno TEXT,
-  PRIMARY KEY(curp)
+CREATE TABLE estado (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nombre TEXT NOT NULL
 );
 
-CREATE TABLE `admin`(
-  id INT NOT NULL,
-  username TEXT,
-  `password` TEXT,
-  PRIMARY KEY(id)
+CREATE TABLE alumno (
+  curp TEXT PRIMARY KEY,
+  nombre TEXT NOT NULL,
+  materno TEXT NOT NULL,
+  paterno TEXT NOT NULL
+);
+
+CREATE TABLE `admin` (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username TEXT NOT NULL,
+  `password` TEXT NOT NULL
 );
 
 ALTER TABLE turno
