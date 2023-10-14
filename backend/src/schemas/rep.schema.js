7a9a7a66eed1cef84029122fdb5f0f9f4510b535
wherethacoffee <whr.thacoff23@gmail.com>
2023-10-14 01:50:23 -0600

    Se modificó el esquema de representante

--- backend/src/schemas/rep.schema.js
+++ backend/src/schemas/rep.schema.js
@@ -1,15 +1,24 @@
 import { z } from "zod";
 
-export const addAdminSchema = z.object({
+const nameRegex = /^[A-Za-záéíóúüñÁÉÍÓÚÜÑ]+ [A-Za-záéíóúüñÁÉÍÓÚÜÑ]+$/
+
+export const addRepSchema = z.object({
     nombre: z.string({
-        required_error: 'Nombre de usuario requerido',
+        required_error: 'Nombre de representante requerido',
+    })
+    .regex(nameRegex, {
+        message: 'Solamente el primer nombre y el primer apellido'
     }),
-    celular: z.number({
+    celular: z.string({
         required_error: 'Celular requerido'
-    }),
-    telefono: z.number({
+    })
+    .min(10, 'El numero debe tener al menos 10 digitos')
+    .max(10, 'El numero debe tener tan solo 10 caracteres'),
+    telefono: z.string({
         required_error: 'Telefono requerido'
-    }),
+    })
+    .min(10, 'El numero debe tener al menos 10 digitos')
+    .max(10, 'El numero debe tener tan solo 10 caracteres'),
     correo: z.string({
         required_error: 'Correo electronico requerido'
     }).email({
