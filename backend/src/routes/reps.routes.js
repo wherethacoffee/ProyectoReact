b47fde45dd5899031412211ad06a1eb788f60b23
wherethacoffee <whr.thacoff23@gmail.com>
2023-10-14 01:49:55 -0600

    Se modificaron las rutas para la validacion

--- backend/src/routes/reps.routes.js
+++ backend/src/routes/reps.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { save, list, update, remove } from "../controllers/reps.controllers.js";
+import { validateSchema } from "../middlewares/validacion.middleware.js";
+import { addRepSchema } from "../schemas/rep.schema.js";
 
 const router = Router();
 
 router.get('/listar', list);
-router.post('/agregar', save);
+router.post('/agregar', validateSchema(addRepSchema), save);
 router.put('/actualizar/:id', update);
 router.delete('/eliminar/:id', remove);
 
