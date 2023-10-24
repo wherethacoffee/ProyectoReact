0f5680186d383e4d73c9b540f2fff0e33bc0a721
wherethacoffee <whr.thacoff23@gmail.com>
2023-10-24 14:33:19 -0600

    se agrego el archivo de configuracion del bakcend

---
+++ backend/src/config.js
@@ -0,0 +1,10 @@
+export const PORT = process.env.PORT || 4000
+
+export const TOKEN_SECRET = process.env.TOKEN_SECRET || 'secret key'
+
+//DB OPTIONS
+export const DB_HOST = process.env.DB_HOST || 'containers-us-west-116.railway.app'
+export const DB_USER = process.env.DB_USER || 'root'
+export const DB_PWD = process.env.DB_PWD || 'QTPUvdiJJgZvZnupfzuh'
+export const DB_NAME = process.env.DB_NAME || 'railway'
+export const DB_PORT = process.env.DB_PORT || 5660
